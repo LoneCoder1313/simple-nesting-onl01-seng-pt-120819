61e177d7a6cfd93eccd1e230b904ef2f2635e53e
Ricky dildy <rdildy92@gmail.com>
2019-12-29 22:56:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchies][:language]
+     programmer_hash[:dennis_ritchies][:language][]
 end
 
 def adding_matz
