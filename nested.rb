eb5c35cf3be2b9c3c87541588f5374ec820b3725
Ricky dildy <rdildy92@gmail.com>
2019-12-29 22:52:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
      }
          puts programmer_hash[:grace_hopper]
 end
-                     :known_for => "COBOL" 
+                     :known_for => "COBOL", :languages "COBOL", "FORTRAN"
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
